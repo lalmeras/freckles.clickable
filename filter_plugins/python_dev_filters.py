54869748988182ab9d0590df6c5168ef057c3ee5
Markus Binsteiner <makkus@posteo.de>
2017-09-14 10:18:02 +0800

    Cleanup imports.

--- filter_plugins/python_dev_filters.py
+++ filter_plugins/python_dev_filters.py
@@ -1,17 +1,4 @@
-import copy
-import fnmatch
 import os
-import pprint
-import subprocess
-from distutils import spawn
-
-from ansible import errors
-from frkl import frkl
-from nsbl.nsbl import ensure_git_repo_format
-from requests.structures import CaseInsensitiveDict
-from six import string_types
-
-import yaml
 
 try:
     set

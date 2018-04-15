0a6d82a8b4e137536997ec7164ab8a3b270670a1
Markus Binsteiner <makkus@posteo.de>
2018-04-15 14:58:14 +1200

    Refactoring: new freckelize

--- filter_plugins/python_dev_filters.py
+++ filter_plugins/python_dev_filters.py
@@ -9,8 +9,13 @@ except NameError:
 class FilterModule(object):
     def filters(self):
         return {
-            'project_name_filter': self.project_name_filter
+            'project_name_filter': self.project_name_filter,
+            'python_dev_get_pkg_mgrs': self.python_dev_get_pkg_mgrs
         }
 
     def project_name_filter(self, project_path):
         return os.path.basename(project_path)
+
+    def python_dev_get_pkg_mgrs(self, profile_vars_folders):
+
+        raise profile_vars_folders
